a52b72a97294b3b0c3b4a0cf656b0a2276f6710c
Splinter Suidman <splinter@mannenopdemaan.nl>
2018-06-30 20:59:00 +0200

    Add support for drag and dropping a file onto the window.

--- src/config.rs
+++ src/config.rs
@@ -17,7 +17,7 @@ impl Config {
         let matches = App::new("game-of-life")
         .version("0.3.0")
         .author("Splinter Suidman")
-        .about("game-of-life emulates John Conway's game of life.\nPress Escape to exit, press C to toggle cursor capture and press Space or a mouse button to reinitialise grid.")
+        .about("game-of-life emulates John Conway's game of life.\nPress Escape to exit, press C to toggle cursor capture and press Space or a mouse button to reinitialise grid.\nA file can be dropped on the screen to load that file. When the grid is reinitialised, it will be reinitialised with that file.")
         .arg(Arg::with_name("width")
             .short("w")
             .long("width")
